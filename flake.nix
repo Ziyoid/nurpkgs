72402eaf9186cc6c2f66f36d7857036f98356bfe
Ziyoid <zepharvision@proton.me>
2025-06-05 11:40:21 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,16 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
       legacyPackages = forAllSystems (system: import ./default.nix {
