75104be8433beb9860a30160502cee01f6ace447
Ziyoid <zepharvision@proton.me>
2025-06-18 21:22:02 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,5 @@
 { pkgs ? import <nixpkgs> { } }:
 
 {
-  config = {
-    allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-       "checkra1n"
-    ];
-  };
-
   palera1n = pkgs.callPackage ./pkgs/palera1n { };
 }
